cf957d77d4fb0d30fd0a4783ae47dd616e3c158d
Peter Pear <peter82pear@gmail.com>
2024-04-22 14:35:00 +0100

    add initial nvim dotfiles

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+local keymap = vim.keymap
+local options = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x"')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
